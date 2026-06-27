6cef85b3c6d9805a2dbbd24df4870018f3206b48
Sanjay <sanjaykambam@gmail.com>
2026-06-27 21:01:19 +0530

    changes to the calculator web app

--- CalculatorWeb/Program.cs
+++ CalculatorWeb/Program.cs
@@ -5,22 +5,23 @@ using CalculatorApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-// Register the existing calculator implementation from referenced project
 builder.Services.AddSingleton<ICalculator, Calculator>();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
+app.UseDefaultFiles();
 
 app.UseStaticFiles();
-app.UseRouting();
 
-app.MapRazorPages();
-
+app.MapGet("/api/calc", (string op, double a, double b, ICalculator calc) =>
+{
+    return Results.Ok(op.ToLowerInvariant() switch
+    {
+        "add" or "+" => calc.Add(a, b),
+        "sub" or "-" => calc.Subtract(a, b),
+        "mul" or "*" => calc.Multiply(a, b),
+        "div" or "/" => calc.Divide(a, b),
+        _ => throw new InvalidOperationException("Unknown operation")
+    });
+});
 app.Run();
